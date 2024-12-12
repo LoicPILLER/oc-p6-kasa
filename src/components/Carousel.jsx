f506a5146302eed7448907e324c3b24caf0b1b31
Loïc PILLER <loic.piller@etu.univ-nantes.fr>
2024-12-12 22:50:57 +0100

    update icons + fix Carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -17,13 +17,28 @@ function Carousel({ picturesArray }) {
         );
     };
 
+    const needButtons = true;
+    if (picturesArray.length === 0) {
+        return null;
+    } else if (picturesArray.length === 1) {
+        return (
+            <div className="carousel">
+                <ul>
+                    <li className="slide active">
+                        <img src={picturesArray[0]} alt="Photo du bien"/>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+
     return (
         <div className="carousel">
             <button className="carousel-button" id="prev" onClick={handlePrev}>
-                <ChevronLeft size={window.innerWidth > 600 ? 80 : 36} color="white" />
+                <ChevronLeft size={window.innerWidth > 600 ? 80 : 36} color="white"/>
             </button>
             <button className="carousel-button" id="next" onClick={handleNext}>
-                <ChevronRight size={window.innerWidth > 600 ? 80 : 36} color="white" />
+                <ChevronRight size={window.innerWidth > 600 ? 80 : 36} color="white"/>
             </button>
             <ul>
                 {picturesArray.map((element, index) => (
@@ -31,10 +46,11 @@ function Carousel({ picturesArray }) {
                         className={`slide ${index === currentIndex ? "active" : ""}`}
                         key={index}
                     >
-                        <img src={element} alt="Photo du bien" />
+                        <img src={element} alt={"Photo " + (currentIndex + 1) + " du bien"}/>
                     </li>
                 ))}
             </ul>
+            <p className="carousel-index">{currentIndex + 1} / {picturesArray.length}</p>
         </div>
     );
 }
